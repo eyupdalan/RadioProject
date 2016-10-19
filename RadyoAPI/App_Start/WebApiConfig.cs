97a1a91e1d33f30a7b2753f895b7ebd4af7d25be
burakhand <burakhand@HLY-BURAKHAND>
2016-10-19 15:14:36 +0300

    api

--- RadyoAPI/App_Start/WebApiConfig.cs
+++ RadyoAPI/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace RadyoAPI
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
             config.Formatters.Remove(config.Formatters.XmlFormatter);
